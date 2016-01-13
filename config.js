9a5b1c1bad9e960bc4467c2d3739e1d885c2168b
Stanislav Kalashnik <sk@infomir.eu>
2016-01-13 18:45:41 +0200

    rework config

--- config.js
+++ config.js
@@ -7,21 +7,27 @@
 
 'use strict';
 
+var path = require('path');
+
 // set of named configs for corresponding gulp tasks
 // each profile inherits all options from the "default" profile
 module.exports = {
-    // config to be extended by other profiles
     default: {
         // directory to look for source files
-        // default: <project root>/src/sass
-        sourcePath: 'sass',
+        sourcePath: path.join(process.env.PATH_SRC, 'sass'),
 
         // main source entry point
         sourceFile: 'main.scss',
 
         // directory to store output files
-        // default: <project root>/app/css
-        targetPath: 'css',
+        targetPath: path.join(process.env.PATH_APP, 'css'),
+
+        // intended output file name
+        targetFile: 'release.css',
+
+        // output format of the final CSS style
+        // options: nested, expanded, compact, compressed
+        outputStyle: 'compressed',
 
         // whether to use space or tab character for indentation
         indentType: 'space',
@@ -33,41 +39,31 @@ module.exports = {
         lineBreak: 'lf',
 
         // how many digits after the decimal will be allowed
-        precision: 2
-    },
-
-    // config for sass:build:develop task
-    develop: {
-        // intended output file name
-        targetFile: 'develop.css',
-
-        // output format of the final CSS style
-        // options: nested, expanded, compact, compressed
-        outputStyle: 'nested',
+        precision: 2,
 
         // additional debugging information in the output file as CSS comments
         sourceComments: false,
 
         // the writing location for the source map file
         // options: file name, true - inline source map, false - disable
-        sourceMap: 'develop.map',
+        sourceMap: false,
 
         // whether to include the source files content in the source map
         // bigger map file but no need to serve source scss files
         sourceMapContents: false,
 
-        // create task sass:watch:develop
+        // create watch task
         // to automatically rebuild on source files change
-        watch: true
+        watch: false
     },
 
-    // config for sass:build:release task
-    release: {
-        // intended output file name
-        targetFile: 'release.css',
+    develop: {
+        targetFile: 'develop.css',
 
-        // output format of the final CSS style
-        // options: nested, expanded, compact, compressed
-        outputStyle: 'compressed'
+        outputStyle: 'nested',
+
+        sourceMap: 'develop.map',
+
+        watch: true
     }
 };
